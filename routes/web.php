110e36377749883418a95aa7f1fd936c01da1d9b
Putra Prima Arhandi <putraprima@gmail.com>
2024-05-18 06:48:43 +0900

    ```text feat: Add GuestBookController for public user

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('guest-books', 'App\Http\Controllers\GuestBookController')
+    ->only(['index']);
