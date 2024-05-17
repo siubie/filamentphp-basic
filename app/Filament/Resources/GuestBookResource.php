38642fc61a836cf4b2e822297a30376e8fcb2ddd
Putra Prima Arhandi <putraprima@gmail.com>
2024-05-17 22:43:24 +0900

    feat: Add TextColumn to GuestBookResource table
    This commit adds the TextColumn to the GuestBookResource table in order to display the name, email, and message columns in the Filament admin panel. This change improves the readability and usability of the guest book table.

--- app/Filament/Resources/GuestBookResource.php
+++ app/Filament/Resources/GuestBookResource.php
@@ -9,6 +9,7 @@ use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -32,6 +33,9 @@ class GuestBookResource extends Resource
         return $table
             ->columns([
                 //
+                TextColumn::make('name'),
+                TextColumn::make('email'),
+                TextColumn::make('message')
             ])
             ->filters([
                 //
