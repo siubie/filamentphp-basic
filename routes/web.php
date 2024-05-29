d79b088f2d8857e7345300e63717330f2b0897c9
Putra Prima Arhandi <putraprima@gmail.com>
2024-05-29 21:10:44 +0900

    add template to web.php

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('template');
 });
 
 Route::resource('guest-book', 'App\Http\Controllers\GuestBookController')
