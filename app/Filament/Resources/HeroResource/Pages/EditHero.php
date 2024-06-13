eb2cacb0e3cd36b79850a6d3be00c7d0331ed43a
Putra Prima <putraprima@gmail.com>
2024-06-13 12:44:32 +0900

    edit hero

--- app/Filament/Resources/HeroResource/Pages/EditHero.php
+++ app/Filament/Resources/HeroResource/Pages/EditHero.php
@@ -3,8 +3,10 @@
 namespace App\Filament\Resources\HeroResource\Pages;
 
 use App\Filament\Resources\HeroResource;
+use App\Models\Hero;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Database\Eloquent\Model;
 
 class EditHero extends EditRecord
 {
@@ -20,5 +22,15 @@ class EditHero extends EditRecord
     public function getRedirectUrl(): string
     {
         return $this->getResource()::getUrl('index');
+    }
+
+    protected function handleRecordUpdate(Model $record, array $data): Model
+    {
+        $record->update($data);
+        //update other hero records to inactive if new hero is active
+        if ($record->is_active) {
+            Hero::where('id', '!=', $record->id)->update(['is_active' => 0]);
+        }
+        return $record;
     }
 }
